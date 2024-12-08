e4e0cfb1dbddd478cbb372a56bab4ecca12e7d8b
BriceLucifer <2376671337@qq.com>
2024-12-08 19:58:02 +0800

    loading bar

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 
-	"github.com/BriceLucifer/CommandLine/cursor"
+	// "github.com/BriceLucifer/CommandLine/cursor"
 	"github.com/BriceLucifer/CommandLine/loading"
 	"github.com/BriceLucifer/CommandLine/readline"
 )
 
 func main() {
-	cursor.Move(cursor.NEXT_LINE, 3)
-	fmt.Printf("------------\n")
-	cursor.Move(cursor.PREV_LINE, 1)
-	fmt.Printf("hello\n")
-	fmt.Printf("xxxxxxxxxx\n--------\n-----xxxxxxddddddddd-\n")
-	cursor.Move(cursor.UP, 1)
-	cursor.Move(cursor.LEFT, 100)
-	fmt.Printf("testing\n")
+	// cursor.Move(cursor.NEXT_LINE, 3)
+	// fmt.Printf("------------\n")
+	// cursor.Move(cursor.PREV_LINE, 1)
+	// fmt.Printf("hello\n")
+	// fmt.Printf("xxxxxxxxxx\n--------\n-----xxxxxxddddddddd-\n")
+	// cursor.Move(cursor.UP, 1)
+	// cursor.Move(cursor.LEFT, 100)
+	// fmt.Printf("testing\n")
 
 
 	loading.LoadingBar()
